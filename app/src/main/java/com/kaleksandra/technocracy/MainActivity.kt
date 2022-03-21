a28b0567b961b6800bb72e1708ce9a3d2581ddf8
l0w0l-Y <portos100.00.1a@gmail.com>
2022-03-21 19:22:50 +0300

    Initial project

---
+++ app/src/main/java/com/kaleksandra/technocracy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kaleksandra.technocracy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
